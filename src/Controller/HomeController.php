6ff9952801ef98adb5edb3fc5c5719124ae7bb34
lbrugiere <114055594+lbrugiere@users.noreply.github.com>
2026-07-31 12:12:12 +0200

    feat: home-side missions (#37)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\MissionRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/',name: 'app_home')]
-    public function index(): Response
+    public function index(MissionRepository $missionRepository): Response
     {
+        $missions = $missionRepository->findOpenMissions();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'missions' => $missions,
         ]);
     }
 }
